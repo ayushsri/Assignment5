b9a4c3e7211ee64dd6357f00be3dc25bde2caaaa
ayushsri <ayush.s0410@gmail.com>
2020-11-19 01:13:00 -0500

    assignment 5

---
+++ ParSort.java
@@ -0,0 +1,70 @@
+ package edu.neu.coe.info6205.sort.par;
+
+ import java.util.Arrays;
+ import java.util.concurrent.CompletableFuture;
+ import java.util.concurrent.ForkJoinPool;
+
+ /**
+ * This code has been fleshed out by Ziyao Qiao. Thanks very much.
+ * TODO tidy it up a bit.
+ */
+class ParSort {
+
+    public static int cutoff = 1000;
+    public static int noOfTheads=1;
+    static ForkJoinPool m = new ForkJoinPool(noOfTheads);
+
+   /* public void setNoOfTheads(int noOfTheads) {
+        this.noOfTheads = noOfTheads;
+    }
+
+    public int getNoOfTheads() {
+        return noOfTheads;
+    }*/
+
+    public static void sort(int[] array, int from, int to) {
+        if (to - from < cutoff) Arrays.sort(array, from, to);
+        else {
+            CompletableFuture<int[]> parsort1 = parsort(array, from, from + (to - from) / 2); // TO IMPLEMENT
+            CompletableFuture<int[]> parsort2 = parsort(array, from + (to - from) / 2, to);// TO IMPLEMENT
+            CompletableFuture<int[]> parsort = parsort1.thenCombine(parsort2, (xs1, xs2) -> {
+                int[] result = new int[xs1.length + xs2.length];
+                // TO IMPLEMENT
+                int i=0, j=0, k=0;
+                int n=xs1.length;
+                int m=xs2.length;
+                while(i !=n  && j != m) {
+                    if(xs1[i] <= xs2[j]) {
+                        result[k++] = xs1[i++];
+                    }else if(xs1[i] > xs2[j]) {
+                        result[k++] = xs2[j++];
+                    }
+                }
+
+                while(i != n) {
+                    result[k++] = xs1[i++];
+                }
+                while(j != m) {
+                    result[k++] = xs2[j++];
+                }
+                return result;
+            });
+
+            parsort.whenComplete((result, throwable) -> System.arraycopy(result, 0, array, from, result.length));
+//            System.out.println("# threads: "+ ForkJoinPool.commonPool().getRunningThreadCount());
+            parsort.join();
+        }
+    }
+
+    private static CompletableFuture<int[]> parsort(int[] array, int from, int to) {
+        return CompletableFuture.supplyAsync(
+                () -> {
+                    int[] result = new int[to - from];
+                    // TO IMPLEMENT
+                    System.arraycopy(array, from, result, 0, result.length);
+                    sort(result, 0, to - from);
+                    return result;
+                },m
+        );
+    }
+}
